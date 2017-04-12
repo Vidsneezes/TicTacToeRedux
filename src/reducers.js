3d435fb09b6b0e72d39761b54d84a4f09224dd1f
Vidsneezes <moraleszez@gmail.com>
2017-04-12 10:58:06 -0600

    added end game case in reducers js

--- src/reducers.js
+++ src/reducers.js
@@ -1,11 +1,13 @@
 import { List, Map } from 'immutable';
 import { CHANGE_GRID, END_GAME } from './actions';
 
-export function gameloop(state = Map(), action){
+export default function gameloop(state = Map(), action){
     switch(action.type){
         case CHANGE_GRID:
             let grid = state.get('grid');
             grid.set(action.indexPosition, value);
             return state.set('grid',grid);
+        case END_GAME:
+            return state.set('done',true);
     }
 }
