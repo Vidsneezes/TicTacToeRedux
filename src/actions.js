37ef2b4fda0373f3bca813f8a0965ec809c3982b
Vidsneezes <moraleszez@gmail.com>
2017-04-12 10:46:15 -0600

    added change grid to action js

--- src/actions.js
+++ src/actions.js
@@ -1,2 +1,13 @@
 import { List, Map } from 'immutable';
 
+export const CHANGE_GRID = "CHANGE_GRID";
+
+const initialState = Map({done:false,grid:List([000000000])});
+
+export function changeGrid(indexPosition, value){
+    return {
+        type: CHANGE_GRID,
+        indexPosition,
+        value
+    }
+}
